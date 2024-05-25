e219a7eec75480856e937b018e4da3d6c8951d58
Gaurav Katiyar <88607215+Gktr1080@users.noreply.github.com>
2024-05-25 05:31:15 +0000

    conversion of datatypes

---
+++ 03_conversionOperations.js
@@ -0,0 +1,25 @@
+let score = 33
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+//"33" = 33 convert ho jayega
+//"33abc" = NaN(not a number)
+//true = 1; false = 0;
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+
+//1 = true; 0=false;
+//" = false"
+//"guarav" = true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
