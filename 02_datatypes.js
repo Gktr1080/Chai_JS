b362cd9ed282219154b7ff0cb97ff8e005057336
Gaurav Katiyar <88607215+Gktr1080@users.noreply.github.com>
2024-05-25 05:10:44 +0000

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,18 @@
+let name = "gaurav katiyar"
+let age = 20
+let state;
+
+//number 2 to the power 52
+// bigint for big number
+//string = ""
+//boolean = true/false
+//null = standalone value
+//undefined
+//symbol = unique
+
+//object
+
+console.log(typeof age);
+
+console.log(typeof null); //null is a object
+console.log(typeof undefined);
