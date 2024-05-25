923e8d46dece352f9bb3d05efb4dfec9f4984a38
Gaurav Katiyar <88607215+Gktr1080@users.noreply.github.com>
2024-05-25 10:13:15 +0530

    Create 01_variables.js

---
+++ 01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345;
+let acoountEmail = "gaurav@gaurav.com";
+var accountPassword = "454545";
+accountcity = "jaipur";
+
+console.log(accountId);
+
+// prefer not to use var because of scope
+
+console.table([accountId,accountPassword,acoountEmail,accountcity]);
